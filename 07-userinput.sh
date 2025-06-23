a6454f7fe98a7c97fa33e345fc401d10740dfdb2
devops-notes-siva <devops.notes.siva@gmail.com>
2025-06-23 21:00:20 +0530

    msg

--- 07-userinput.sh
+++ 07-userinput.sh
@@ -2,6 +2,12 @@
 
 #echo "enter your name"
 #read name
-read -p "enter your name" name
+read -p "enter your name:  " name
+rep -s -p "enter password:  " password
 
 echo "hi $name, good day"
+echo "password is : $password"
+
+#timed inputs
+read -t 5 -p "echo within time of 5  secinds" time
+echo "time input is $time"
