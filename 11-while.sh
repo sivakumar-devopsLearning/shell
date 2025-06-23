b7a8e16dd83626cd542a0074557aae28162c41c5
devops-notes-siva <devops.notes.siva@gmail.com>
2025-06-23 22:41:52 +0530

    msg

--- 11-while.sh
+++ 11-while.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 counter=1
 
-while [ $counter -le 10 ]; do
+while [ $counter -ge 10 ]; do
     echo "number is $counter"
     counter=$(( counter+1 ))
 done
 
 echo "loop ended"
+
+# for until loop use until in place of while
---
+++ 12-functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+print_args() {
+
+    echo "fiest arg: $1"
+    echo "secong arg is: $2"
+
+}
+
+print_args "hi" "how are you"
