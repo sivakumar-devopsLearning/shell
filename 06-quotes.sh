9d33ba88d8b0807e78eeeaf99b4f840558e9f607
devops-notes-siva <devops.notes.siva@gmail.com>
2025-06-23 20:45:56 +0530

    msg

--- 06-quotes.sh
+++ 06-quotes.sh
@@ -7,3 +7,11 @@ echo 'single quotes: $name'
 echo no quotes: $name
 echo "both double start: '$name' "
 echo 'both single start: "$name" '
+
+#escape charcter for key words
+
+special_word="\$"
+echo "varb is $special_word"
+
+special_key="$"
+echo "vars1 $special_key"
