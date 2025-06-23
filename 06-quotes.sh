c3c2438dbd8b66206bd169d173885b6f5a79bce2
devops-notes-siva <devops.notes.siva@gmail.com>
2025-06-23 20:35:11 +0530

    msg

---
+++ 06-quotes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="siva kumar"
+
+echo "double quotes: $name"
+echo 'single quotes: $name'
+echo no quotes: $name
+echo "both double start: '$name' "
+echo 'both single start: "$name" '
