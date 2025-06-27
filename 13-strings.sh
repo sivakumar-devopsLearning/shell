4aefd5790311faa8c959302683eead76e8cc686d
devops-notes-siva <devops.notes.siva@gmail.com>
2025-06-27 08:46:01 +0530

    msg

--- 13-strings.sh
+++ 13-strings.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 
-string1="Hello,  world! are you doing good"
-substring1=${string1:7:5}
+string1="Hello, world! are you doing good"
+substring1=${string1:7:5} #counts space also and prints from 8th word
 # word extract "world!"
 echo "substring is $substring1"
 
 ################################################
 
 string2="Heloo world, heloo how are you, heloo how do you do"
-search="Heloo"
+search="Heloo" # replace eactly same word Heloo, not heloo
 replace="hiiii"
 #replacement of string
 result=${string2//$search/$replace}
